c49adb5c0d8904948937124ae515a8cf8d69fcf3
sala 105 <sala105@poliacademico.edu.loc>
2026-02-05 19:18:22 -0500

    primer archivo de repositorio

---
+++ app.py
@@ -0,0 +1,7 @@
+class Estudiante:
+    def __init__(self, nombre, documento):
+        self.nombre = nombre
+        self.docuemtno = documento
+
+estudiante1 = Estudiante("Juan", 1111)
+print(estudiante1.nombre)
